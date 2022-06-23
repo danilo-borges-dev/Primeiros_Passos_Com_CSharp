112608875f88c7a1e4ad677a6a3faecaf1640b2f
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-06-23 09:59:04 -0300

    Adicionado o primeiro projeto

---
+++ MeuPrimeiroProgramaCSharp/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+        }
+    }
+}
